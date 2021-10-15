69dfd7443a4848c869106bce21d4befc6ac2b0a3
Hagane26 <haganeizzy@gmail.com>
2021-10-15 20:54:53 +0700

    Test Android

--- V1/app/src/main/java/com/example/onschool/MainActivity.java
+++ V1/app/src/main/java/com/example/onschool/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.onschool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv1 = this.findViewById(R.id.tv1);
+        Button btn1 = this.findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
---
+++ V1/app/src/test/java/WorkBackground.java
@@ -0,0 +1,7 @@
+public class WorkBackground {
+
+    public static String Login(String u_email, String u_pass){
+        String va = u_email.toString() + " " + u_pass.toString();
+        return va;
+    }
+}
