014b318a41a5bf1c24ab87adea1a7de21ce54815
Anta Akbar <antaakbar.05@gmail.com>
2021-10-17 21:57:07 +0700

    layout_signin.xml

--- V1/app/src/main/java/com/example/onschool/MainActivity.java
+++ V1/app/src/main/java/com/example/onschool/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_lokakarya);
+        setContentView(R.layout.layout_landing);
 
     }
 }
