ba2a656f2791487f711785407044e9047bc69d4d
Hagane <jharliangga26@gmail.com>
2022-02-07 19:39:16 +0700

    Perbaikan SignUp

--- V1/app/src/main/java/com/example/onschool/SignupActivity.java
+++ V1/app/src/main/java/com/example/onschool/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.onschool;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
     @Override
@@ -24,19 +35,73 @@ public class SignupActivity extends AppCompatActivity {
 
         Button btn_register = findViewById(R.id.btn_register);
 
-        String email = tb_email.getText().toString().trim();
-        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.matches(emailPattern)){
-                    Toast.makeText(SignupActivity.this, "Match\n" + email, Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(SignupActivity.this, "No Match\n" + email, Toast.LENGTH_SHORT).show();
-                }
+                checkField(SignupActivity.this,
+                        tb_namaDepan.getText().toString(),
+                        tb_namaBelakang.getText().toString(),
+                        tb_email.getText().toString(),
+                        tb_username.getText().toString(),
+                        tb_password.getText().toString());
             }
         });
-
     }
+
+    public Boolean EmailValidation(String email){
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (email.matches(emailPattern)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void checkField(Context ctx, String ND, String NB,String email, String Username, String Password){
+        if (ND.equals(null)){
+            Toast.makeText(ctx, "Field Nama Depan Kosong", Toast.LENGTH_SHORT).show();
+        }else{
+            if(NB.equals(null)){
+                Toast.makeText(ctx, "Field Nama Belakang Kosong", Toast.LENGTH_SHORT).show();
+            }else{
+                if(EmailValidation(email).equals(false)){
+                    Toast.makeText(ctx, "Email tidak Valid", Toast.LENGTH_SHORT).show();
+                }else{
+                   if (ND.equals(null) || NB.equals(null)){
+                       Toast.makeText(ctx, "Field username atau password kosong", Toast.LENGTH_SHORT).show();
+                   }else{
+                       Toast.makeText(ctx, "Berhasil", Toast.LENGTH_SHORT).show();
+                   }
+                }
+            }
+        }
+    }
+
+    public void jsonParse(Context ctx, String ND,String NB, String email, String Username, String Password){
+        String url = "";
+        RequestQueue rq = Volley.newRequestQueue(ctx);
+
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> params = new HashMap<>();
+
+                return params;
+            }
+        };
+        rq.add(sr);
+    }
+
 }
