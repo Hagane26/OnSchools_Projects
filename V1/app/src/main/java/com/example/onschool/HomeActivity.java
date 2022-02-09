34ce2b8a6f4f33ea7a7ad0a0b5144ca1fbf2fb4c
Anta Akbar <antaakbar.05@gmail.com>
2022-02-09 15:16:32 +0700

    Memperbaiki layout_lokakarya.xml

--- V1/app/src/main/java/com/example/onschool/HomeActivity.java
+++ V1/app/src/main/java/com/example/onschool/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.example.onschool;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_home);
+        setContentView(R.layout.layout_lokakarya);
 
         sessionManager = new SessionManager(this);
         sessionManager.checkLogin();
