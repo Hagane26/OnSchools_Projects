a78b2d3bab0d920a462cfd67a7f624ec43a99422
Hagane <jharliangga26@gmail.com>
2022-02-18 01:30:46 +0700

    Register Berhasil, note: tanpa google account tanpa twitter tanpa facebook

--- V1/app/src/main/java/com/example/onschool/SignupActivity.java
+++ V1/app/src/main/java/com/example/onschool/SignupActivity.java
@@ -1,9 +1,12 @@
 package com.example.onschool;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextPaint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,8 +34,7 @@ public class SignupActivity extends AppCompatActivity {
         EditText tb_namaDepan = findViewById(R.id.tbx_nama_depan);
         EditText tb_namaBelakang = findViewById(R.id.tbx_nama_belakang);
         EditText tb_email = findViewById(R.id.tbx_email);
-        EditText tb_username = findViewById(R.id.tbx_username);
-        EditText tb_password = findViewById(R.id.tbx_password_signup);
+               EditText tb_password = findViewById(R.id.tbx_password_signup);
 
         Button btn_register = findViewById(R.id.btn_register);
 
@@ -42,7 +47,6 @@ public class SignupActivity extends AppCompatActivity {
                         tb_namaDepan.getText().toString(),
                         tb_namaBelakang.getText().toString(),
                         tb_email.getText().toString(),
-                        tb_username.getText().toString(),
                         tb_password.getText().toString());
             }
         });
@@ -57,7 +61,7 @@ public class SignupActivity extends AppCompatActivity {
         }
     }
 
-    public void checkField(Context ctx, String ND, String NB,String email, String Username, String Password){
+    public void checkField(Context ctx, String ND, String NB,String email,  String Password){
         if (ND.equals(null)){
             Toast.makeText(ctx, "Field Nama Depan Kosong", Toast.LENGTH_SHORT).show();
         }else{
@@ -70,20 +74,37 @@ public class SignupActivity extends AppCompatActivity {
                    if (ND.equals(null) || NB.equals(null)){
                        Toast.makeText(ctx, "Field username atau password kosong", Toast.LENGTH_SHORT).show();
                    }else{
-                       Toast.makeText(ctx, "Berhasil", Toast.LENGTH_SHORT).show();
+                       jsonParse(ctx,ND,NB,email,Password);
                    }
                 }
             }
         }
     }
 
-    public void jsonParse(Context ctx, String ND,String NB, String email, String Username, String Password){
-        String url = "";
+    public void jsonParse(Context ctx, String ND,String NB, String email,  String Password){
+        String url = "http://onschool.id/api/register";
         RequestQueue rq = Volley.newRequestQueue(ctx);
 
         StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                try {
+                    // Semua Data Respon
+                    JSONObject jo = new JSONObject(response);
+                    String metaInfo = jo.getString("meta");     // data respon bagian meta
+                    String dataInfo = jo.getString("data");     // data respon bagian data
+
+                    JSONObject meta = new JSONObject(metaInfo);         // jadikan object untuk bagian meta
+                    JSONObject data = new JSONObject(dataInfo);         // jadikan object untuk bagian data
+
+                    // pengambilan informasi meta
+                    String meta_status = meta.getString("status");
+
+                    Toast.makeText(ctx, "Register " + meta_status, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(ctx,LoginActivity.class));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }, new Response.ErrorListener() {
@@ -95,7 +116,11 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String,String> params = new HashMap<>();
-
+                params.put("nama",ND + " " + ND);
+                params.put("asal_sekolah","t");
+                params.put("alamat","t");
+                params.put("email",email);
+                params.put("password",Password);
                 return params;
             }
         };
