0bf2e8156ceaf0faba04fe57b2e1702987c2eacc
Hagane <jharliangga26@gmail.com>
2022-02-20 01:58:55 +0700

    SignUp with Goolge part1

--- V1/app/src/main/java/com/example/onschool/SignupActivity.java
+++ V1/app/src/main/java/com/example/onschool/SignupActivity.java
@@ -1,5 +1,7 @@
 package com.example.onschool;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.TextPaint;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +21,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,6 +36,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 201;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +46,10 @@ public class SignupActivity extends AppCompatActivity {
         EditText tb_namaDepan = findViewById(R.id.tbx_nama_depan);
         EditText tb_namaBelakang = findViewById(R.id.tbx_nama_belakang);
         EditText tb_email = findViewById(R.id.tbx_email);
-               EditText tb_password = findViewById(R.id.tbx_password_signup);
+        EditText tb_password = findViewById(R.id.tbx_password_signup);
 
         Button btn_register = findViewById(R.id.btn_register);
-
+        SignInButton signInButtonGoogle = findViewById(R.id.btn_google);
 
 
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +62,45 @@ public class SignupActivity extends AppCompatActivity {
                         tb_password.getText().toString());
             }
         });
+
+        signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(SignupActivity.this, gso);
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(SignupActivity.this, "-> " + account.getEmail().toString() + "\n" + account.getDisplayName().toString() , Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e(TAG, "signInResult:failed code=" + e.getMessage());
+        }
     }
 
     public Boolean EmailValidation(String email){
